3116e21dbe1fcf63b4a1f7171a5f66b6bdeaf082
David Llanos López <davidllanos22@gmail.com>
2016-10-01 12:39:08 +0200

    Add sprite sheet object to support different sizes. Basic Idea and first drawing of the character.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-##GBJAM5
+##Mafia
 
-Entry for GBJAM5 https://itch.io/jam/gbjam-5
+Entry for GBJAM5 https://itch.io/jam/gbjam-5. I'm making a mafia game.
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var body = WIZARD.physics.createAABB(0, 55, 16, 16);
+var body = WIZARD.physics.createAABB(32, 55, 16, 16);
 var body2 = WIZARD.physics.createAABB(100, 48, 32, 32);
 
 var speed = 0.2;
@@ -9,10 +9,10 @@ wizard({
     scale: 3,
     pixelArt: true,
     create: function(){
-        this.loadImages("bw.png", "spritesheet.png");
+        this.loadImages("spritesheet.png");
         this.loadSounds("sound01.wav");
-        WIZARD.animation.createFrameAnimation("random", [[0,0], [1,0], [0,1], [1,1]], 500);
-        WIZARD.animation.createFrameAnimation("random2", [[0,0], [1,0], [0,1], [1,1]], 200);
+        WIZARD.spritesheet.create("spritesheet", "spritesheet", 16, 32);
+        WIZARD.animation.createFrameAnimation("random", [[0,0]], 500);
     },
 
     update: function(){
@@ -21,22 +21,20 @@ wizard({
         if(body.y > this.height) body.y = - 16;
         else if(body.y < - 16) body.y = this.height;
 
-        if(WIZARD.input.keyPressed(WIZARD.keys.A)){
+        if(WIZARD.input.keyPressed(WIZARD.keys.A) || WIZARD.input.keyPressed(WIZARD.keys.LEFT)){
             body.x -= speed;
-        }else if(WIZARD.input.keyPressed(WIZARD.keys.D)){
+        }else if(WIZARD.input.keyPressed(WIZARD.keys.D) || WIZARD.input.keyPressed(WIZARD.keys.RIGHT)){
             body.x += speed;
-        }if(WIZARD.input.keyPressed(WIZARD.keys.W)){
+        }if(WIZARD.input.keyPressed(WIZARD.keys.W) || WIZARD.input.keyPressed(WIZARD.keys.UP)){
             body.y -= speed;
-        }if(WIZARD.input.keyPressed(WIZARD.keys.S)){
+        }if(WIZARD.input.keyPressed(WIZARD.keys.S) || WIZARD.input.keyPressed(WIZARD.keys.DOWN)){
             body.y += speed;
         }
     },
     render: function(){
         this.clear("#686868");
-        this.drawImage("bw", body.x, body.y);
         //this.drawSprite("spritesheet", 16, 16, 1, 1);
-        this.drawAnimation("spritesheet", "random", 16, 16);
-        this.drawAnimation("spritesheet", "random2", 32, 16);
+        this.drawAnimation("spritesheet", "random", body.x, body.y);
         if(WIZARD.physics.intersects(body, body2)){
             this.drawAABB(body, "#ff0000");
             this.drawAABB(body2, "#ff0000");
